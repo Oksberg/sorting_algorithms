6f68e390cd3c069de2ce772a3b47087403db57c1
Okyere <oksberg89@gmail.com>
2023-11-22 09:19:42 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,11 +25,10 @@ void bubble_sort(int *array, size_t size)
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 					flag = 1;
+					print_array(array, size);
 				}
 			}
-			if (flag != 0)
-				print_array(array, size);
-			else
+			if (flag == 0)
 				break;
 		}
 	}
